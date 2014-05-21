f199f589b78955d0cb748bc689cb67153eae0d59
alejanderl <janderl@gmail.com>
2014-05-21 10:52:27 +0200

    mend

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,23 +1,18 @@
 #encoding: utf-8
 require 'spec_helper'
 
-describe "Testing Index Crawler"   do
+describe "Testing form"   do
 
 
 
-  it "fill up the form and get a request with no results"  do
+  it "fill up the form and get a proper request" , :js do
 
     visit root_path
-    fill_in "wall_offer_uid", :with => "player1"
-    fill_in "wall_offer_pub0", :with => "campaign1"
-    fill_in "wall_offer_page", :with => "1"
-    click_button "Request wall offers"
+    fill_in "amazon_url", :with => "http://www.amazon.com/gp/product/0465050654/ref=s9_psimh_gw_p14_d3_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=center-2&pf_rd_r=1Z4TWWMB46JZTNAH9DVN&pf_rd_t=101&pf_rd_p=1688200382&pf_rd_i=507846"
 
-    page.should have_content "Results"
-    page.should have_content "No offers"
 
   end
 
-  pending "fill up the wall offer form and get results"
+  pending "another feature"
 
 end
